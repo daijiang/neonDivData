a560d8e3e59736097ceb2ead392484592070a0cc
Daijiang Li <dli1@ufl.edu>
2020-12-04 13:21:37 -0600

    update tick pathogen doc

--- R/data_tick_pathogen.R
+++ R/data_tick_pathogen.R
@@ -2,6 +2,15 @@
 #'
 #' This dataset was derived from [NEON data portal](https://data.neonscience.org) with data product ID 'DP1.10092.001'. Details about this data product can be found at <https://data.neonscience.org/data-products/DP1.10092.001>.
 #'
+#' To clean the data, we:
+#'
+#' 1. Removed samples with flagged quality checks
+#' 2. Removed samples with missing metadata (domain/site/plotID; Lat/Long; plotType; nlcdClass, elevation, collectDate, subsampleID, batchID, testingID, testPathogenName
+#' 3. Removed samples where sampleCondition!=“OK”
+#' 4. Removed samples where testResult==NA
+#' 5. Removed samples where HardTick DNA Quality (under testPathogenName) are not ‘Positive’, and then removed hardtack DNA and Ixodes pacificus tests.
+#' 6. Combined B. burgdeferi and B burgdeferi sensu lato into a single test pathogen type (B. burgedferi sensu lato)
+#'
 #' @note  Details of locations (e.g. latitude/longitude coordinates can be found in [neon_locations]).
 #' @format A data frame (also a tibble) with the following columns:
 #'
