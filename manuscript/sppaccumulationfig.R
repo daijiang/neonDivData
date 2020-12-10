b87da087c82fe45d1e3f75b47ead39ddf78a5209
Sydne Record <srecord@brynmawr.edu>
2020-12-10 13:59:07 -0500

    Beetle rarefaction figure
    - finished analysis for figure and created rarefaction curves
    - wrote pdf of figure to Figures folder

--- manuscript/sppaccumulationfig.R
+++ manuscript/sppaccumulationfig.R
@@ -17,15 +17,45 @@ ORNLbeetle <- data_beetle %>%
 # See what date range of sampling has been (2014-2020)
 range(ORNLbeetle$collectDate)
 
-# Get number of individuals across entire sampling time ranked by family
-f <- ORNLbeetle %>%
-  filter(taxonRank == "family")
-
 # Get number of individuals across entire sampling time ranked by genus
-g <- ORNLbeetle %>%
-  filter(taxonRank == "genus")
+gtosubsp <- ORNLbeetle %>%
+  filter(taxonRank == "genus" | taxonRank == "species" | taxonRank == "subspecies")
 
-# Get number of individuals across entire sampling time ranked by species   
-sp <- ORNLbeetle %>%
-  filter(taxonRank == "species")
+# create a vector of genus names for all records from ORNL and append to gtosubsp dataframe
+genus <- rep(NA, length = dim(gtosubsp)[1])
+for(i in 1:dim(gtosubsp)[1]){
+  genus[i] <- strsplit(gtosubsp$scientificName[i], ' ')[[1]][1]
+}
+gtosubsp <- cbind(gtosubsp, genus)
 
+# create a vector of species names for all records from ORNL and append to gtosubsp dataframe
+species <- rep(NA, length = dim(gtosubsp)[1])
+for(i in 1:dim(gtosubsp)[1]){
+  species[i] <- strsplit(gtosubsp$scientificName[i], ' ')[[1]][2]
+}
+gtosubsp <- cbind(gtosubsp, species)
+
+# Create vectors of abundances by genus, species, and subspecies
+by_genus <- gtosubsp %>%
+  group_by(genus) %>%
+  summarise(count=n())
+
+by_species <- gtosubsp %>%
+  group_by(species) %>%
+  summarise(count=n())
+
+by_subspecies <- gtosubsp %>%
+  group_by(scientificName) %>%
+  summarise(count=n())
+
+# Put taxonomic rank by abundance summaries into a list as required by iNEXT functions
+by_rank <- list(genus = by_genus$count, species = by_species$count, subspecies = by_subspecies$count)
+
+# Create iNEXT object for ORNL beetle data
+out <- iNEXT(by_rank, q=c(0,1,2), datatype="abundance")
+
+# Plot species diversity of orders q=0,1,2 for different taxnomic subsets                 
+beetle_rarefac <- ggiNEXT(out, type=1, facet.var="site")
+
+# Export pdf to GitHub
+ggsave("~/GitHub/neonDivData/manuscript/figures/beetle_rarefaction.pdf", plot = beetle_rarefac, width = 10, height = 7)
